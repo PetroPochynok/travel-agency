b987d754ef908a3b919fbedcfb8245ed52f4934a
Petro Pochynok <p.pochynok123@gmail.com>
2026-02-11 13:41:26 +0200

    small fix

--- src/main/java/com/epam/finaltask/dto/UpdatePasswordRequest.java
+++ src/main/java/com/epam/finaltask/dto/UpdatePasswordRequest.java
@@ -1,5 +1,6 @@
 package com.epam.finaltask.dto;
 
+import com.epam.finaltask.annotation.Sensitive;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
@@ -8,14 +9,17 @@ import lombok.Data;
 public class UpdatePasswordRequest {
 
     @NotBlank(message = "{user.password.invalid}")
+    @Sensitive
     private String currentPassword;
 
     @Pattern(
             regexp = "^.{5,}$",
             message = "{user.password.invalid}"
     )
+    @Sensitive
     private String newPassword;
 
     @NotBlank(message = "{user.confirmPassword.required}")
+    @Sensitive
     private String confirmPassword;
 }
